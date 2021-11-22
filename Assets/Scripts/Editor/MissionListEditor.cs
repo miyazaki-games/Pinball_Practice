50702537d3c6f9c8afdc64ca217d8854dc1cb3e2
miyazaki-games <coolcreator0310@gmail.com>
2021-11-22 22:24:32 +0900

    Update

--- Assets/Scripts/Editor/MissionListEditor.cs
+++ Assets/Scripts/Editor/MissionListEditor.cs
@@ -26,5 +26,59 @@ public class MissionListEditor : Editor
     {
         //base.OnInspectorGUI();
         getTarget.Update();
+
+        //LIST SIZE
+        EditorGUILayout.LabelField("Mission List Size");
+        listSize = theList.arraySize;
+        listSize = EditorGUILayout.IntField("List Size", listSize);
+
+        if (listSize != theList.arraySize)
+        {
+            while (listSize > theList.arraySize)
+            {
+                theList.InsertArrayElementAtIndex(theList.arraySize);
+            }
+
+            while (listSize < theList.arraySize)
+            {
+                theList.DeleteArrayElementAtIndex(theList.arraySize - 1);
+            }
+        }
+
+        while (newShowFoldout.Count > theList.arraySize)
+        {
+            newShowFoldout.RemoveAt(newShowFoldout.Count - 1);
+        }
+
+        while (newShowFoldout.Count < theList.arraySize)
+        {
+            newShowFoldout.Add(true);
+        }
+
+        if (GUILayout.Button("Add New Mission"))
+        {
+
+        }
+
+        for (int i = 0; i < theList.arraySize; i++)
+        {
+            EditorGUI.indentLevel++;
+            EditorGUILayout.BeginVertical(GUI.skin.box);
+            newShowFoldout[i] = EditorGUILayout.Foldout(newShowFoldout[i], "Mission" + (i + 1));
+
+            SerializedProperty myListRef = theList.GetArrayElementAtIndex(i);
+            SerializedProperty myId = myListRef.FindPropertyRelative("missionId");
+
+            if (newShowFoldout[i])
+            {
+                EditorGUILayout.PropertyField(myId);
+            }
+
+            EditorGUI.indentLevel--;
+            EditorGUILayout.EndVertical();
+            EditorGUILayout.Separator();
+        }
+
+        getTarget.ApplyModifiedProperties();
     }
 }
